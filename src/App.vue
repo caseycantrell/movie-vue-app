8038aeef29ffb025b852e8bab4c9d64d6f20bce7
Casey <cantrellcasey@gmail.com>
2021-10-19 17:15:54 -0500

    adds bootstrap grid and cards to movies index

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,49 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link>
-      |
-      <router-link to="/about">About</router-link>
-      |
-      <router-link to="/movies">All Movies</router-link>
-      |
-      <router-link to="/movies/new/">New Movie</router-link>
-      |
-      <router-link to="/actors">All Actors</router-link>
-      |
-      <router-link to="/actors/new/">New Actor</router-link>
-      |
-      <span v-if="isLoggedIn()">
-        <router-link to="/logout">Logout</router-link>
-      </span>
-      <span v-else>
-        <router-link to="/signup">Signup</router-link>
-        |
-        <router-link to="/login">Login</router-link>
-      </span>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container-fluid">
+        <router-link class="navbar-brand" to="/">Movies</router-link>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavAltMarkup"
+          aria-controls="navbarNavAltMarkup"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div class="navbar-nav">
+            <li class="nav-item">
+              <router-link class="nav-link" to="/movies">All Movies</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" to="/movies/new">New Movie</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" to="/actors">All Actors</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link class="nav-link" to="/actors">New Actor</router-link>
+            </li>
+            <li v-if="!isLoggedIn()" class="nav-item">
+              <router-link class="nav-link" to="/signup">Signup</router-link>
+            </li>
+            <li v-if="!isLoggedIn()" class="nav-item">
+              <router-link class="nav-link" to="/login">Login</router-link>
+            </li>
+            <li v-if="isLoggedIn()" class="nav-item">
+              <router-link class="nav-link" to="/logout">Logout</router-link>
+            </li>
+          </div>
+        </div>
+      </div>
+    </nav>
+    <div class="container">
+      <router-view />
     </div>
-    <router-view />
   </div>
 </template>
 
--- src/views/MoviesIndex.vue
+++ src/views/MoviesIndex.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="movies-index">
     <img src="https://c.tenor.com/OxIKfikF9IsAAAAM/s%C3%B3observo-watchin-movie.gif" />
+    <br />
+    <br />
     <h1 class="animate__animated animate__lightSpeedInRight animate__slower">All Movies</h1>
     Search by title:
     <input v-model="titleFilter" list="titles" />
@@ -10,18 +12,32 @@
     <datalist id="titles">
       <option v-for="movie in movies" v-bind:key="movie.id">{{ movie.title }}</option>
     </datalist>
+
     <button v-on:click="sortAttribute = 'title'">Sort Alphabetically</button>
     &nbsp;
     <button v-on:click="sortAttribute = 'year'">Sort By Year</button>
-    <div
-      is="transition-group"
-      appear
-      enter-active-class="animate__animated animate__flipInX animate__slow"
-      leave-active-class="animate__animated animate__fadeOutRight"
-    >
-      <div v-for="movie in orderBy(filterBy(movies, titleFilter, 'title'), sortAttribute)" v-bind:key="movie.id">
-        <h4>{{ movie.title }}</h4>
-        <router-link :to="`/movies/${movie.id}`">See Details</router-link>
+    <br />
+    <br />
+    <div class="container">
+      <div class="row row-cols-3">
+        <div
+          class="col"
+          v-for="movie in orderBy(filterBy(movies, titleFilter, 'title'), sortAttribute)"
+          v-bind:key="movie.id"
+        >
+          <div class="card" style="width: 18rem">
+            <img
+              src="https://www.gannett-cdn.com/presto/2020/08/24/PPYD/85a5fe8f-7328-43bf-a7db-da0460ec683d-ENTER-MOVIE-AMC-THEATRES-REOPEN-1-TB.JPG"
+              class="card-img-top"
+              alt=""
+            />
+            <div class="card-body">
+              <h5 class="card-title">{{ movie.title }}</h5>
+              <p class="card-text">Plot: {{ movie.plot }}</p>
+              <router-link class="btn btn-primary" :to="`/movies/${movie.id}`">More Details</router-link>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
